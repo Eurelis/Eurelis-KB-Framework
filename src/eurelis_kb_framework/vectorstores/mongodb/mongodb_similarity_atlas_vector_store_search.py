123f6fb1da36a8e0c98c258649f7fe26d81bef22
Jérôme Diaz <j.diaz@eurelis.com>
2024-01-19 16:55:25 +0100

    update dependencies

--- src/eurelis_kb_framework/vectorstores/mongodb/mongodb_similarity_atlas_vector_store_search.py
+++ src/eurelis_kb_framework/vectorstores/mongodb/mongodb_similarity_atlas_vector_store_search.py
@@ -2,7 +2,7 @@ from typing import Optional, Dict, List, Tuple, Callable, Any
 
 from bson import ObjectId
 from langchain.schema import Document
-from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 
 
 class MongoDBSimilarityAtlasVectorStoreSearch(MongoDBAtlasVectorSearch):
@@ -10,42 +10,29 @@ class MongoDBSimilarityAtlasVectorStoreSearch(MongoDBAtlasVectorSearch):
     Class to enable similarity search with score on mongodb
     """
 
-    def similarity_search_with_score(
-        self,
-        query: str,
-        k: int = 4,
-        *,
-        pre_filter: Optional[Dict] = None,
-        post_filter_pipeline: Optional[List[Dict]] = None,
-    ) -> List[Tuple[Document, float]]:
-        """Override to fix an issue
-        TypeError: MongoDBAtlasVectorSearch.similarity_search_with_score() takes 2 positional arguments but 3 were given
-
-        Args:
-            query:
-            k:
-            pre_filter:
-            post_filter_pipeline:
-
-        Returns:
-
-        """
-        return super().similarity_search_with_score(
-            query, k=k, pre_filter=pre_filter, post_filter_pipeline=post_filter_pipeline
-        )
-
-    def _select_relevance_score_fn(self) -> Callable[[float], float]:
-        """
-        The 'correct' relevance function
-        may differ depending on a few things, including:
-        - the distance / similarity metric used by the VectorStore
-        - the scale of your embeddings (OpenAI's are unit normed. Many others are not!)
-        - embedding dimensionality
-        - etc.
-
-        Vectorstores should define their own selection based method of relevance.
-        """
-        return lambda x: x
+    # def similarity_search_with_score(
+    #     self,
+    #     query: str,
+    #     k: int = 4,
+    #     *,
+    #     pre_filter: Optional[Dict] = None,
+    #     post_filter_pipeline: Optional[List[Dict]] = None,
+    # ) -> List[Tuple[Document, float]]:
+    #     """Override to fix an issue
+    #     TypeError: MongoDBAtlasVectorSearch.similarity_search_with_score() takes 2 positional arguments but 3 were given
+    #
+    #     Args:
+    #         query:
+    #         k:
+    #         pre_filter:
+    #         post_filter_pipeline:
+    #
+    #     Returns:
+    #
+    #     """
+    #     return super().similarity_search_with_score(
+    #         query, k=k, pre_filter=pre_filter, post_filter_pipeline=post_filter_pipeline
+    #     )
 
     def add_documents(self, documents: List[Document], **kwargs: Any) -> List[str]:
         """Run more documents through the embeddings and add to the vectorstore.
@@ -84,8 +71,6 @@ class MongoDBSimilarityAtlasVectorStoreSearch(MongoDBAtlasVectorSearch):
 
         if not ids:
             return True
-
-        print(kwargs)
 
         del_query = {"_uid": {"$in": ids}}
         self._collection.delete_many(del_query)
